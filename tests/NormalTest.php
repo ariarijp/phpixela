833a0a32b9d64cbbbb5b93a6c38201e6175285ba
mitukiti11 <buffalo.rage@gmail.com>
2018-11-02 14:42:53 +0900

    fix unit test for travice ci 'pararel execute, same username'

--- tests/NormalTest.php
+++ tests/NormalTest.php
@@ -17,38 +17,62 @@ class NormalTest extends TestCase
     const token = "CC517a6943346e30474e66594e49782b6f69557643673dCC";
 
     /** @var $user_name_cd string */
-    const user_name_cd = "phpixela-create-delete-tester";
+    const user_name_cd = "phpixela-c-d-tester";
     /** @var $cd_token string */
     const cd_token = "61434c4d6f2b6d6748667856577537674a4c70596c673d3d";
     /** @var $cd_token_reset string */
     const cd_token_reset = "44517a6943346e30474e66594e49782b6f69557643673d3d";
+
+
+    /**
+     * getUser
+     * return string username(travisci safe)
+     */
+    public static function getUserName() {
+        $base_name = self::user_name;
+        $php_version = str_replace('.', '-', phpversion());
+        $user_name = "{$base_name}-{$php_version}";
+        return "{$base_name}-{$php_version}";
+    }
+
+    /**
+     * getUserCd
+     * return string username(travisci safe)
+     */
+    public static function getUserNameCd() {
+        $base_name = self::user_name_cd;
+        $php_version = str_replace('.', '-', phpversion());
+        $user_name = "{$base_name}-{$php_version}";
+        return $user_name;
+    }
 
     /**
      * @test UserAPI ( create / update / delete )
      */
     public function api_user ()
     {
+        $user_name_cd = self::getUserNameCd();
         $client = new Client(self::cd_token);
         $client_reset = new Client(self::cd_token_reset);
 
         try {
             // create
-            $response = $client->createUser(self::cd_token, self::user_name_cd, 'yes', 'yes');
+            $response = $client->createUser(self::cd_token, $user_name_cd, 'yes', 'yes');
             $this->assertTrue($response['isSuccess'], 'API: createUser');
 
             // update 1
-            $response = $client->updateUser(self::user_name_cd, self::cd_token_reset);
+            $response = $client->updateUser($user_name_cd, self::cd_token_reset);
             $this->assertTrue($response['isSuccess'], 'API: updateUser 1');
 
             // update 2
-            $response = $client_reset->updateUser(self::user_name_cd, self::cd_token);
+            $response = $client_reset->updateUser($user_name_cd, self::cd_token);
             $this->assertTrue($response['isSuccess'], 'API: updateUser 2');
 
         } catch (\Throwable $e) {
             $this->fail($e);
         } finally {
             // delete
-            $response = $client->deleteUser(self::user_name_cd);
+            $response = $client->deleteUser($user_name_cd);
             $this->assertTrue($response['isSuccess'], 'API: deleteUser');
         }
     }
@@ -60,9 +84,9 @@ class NormalTest extends TestCase
     {
         $client = new Client(self::token);
         try {
-            $client->getGraphs(self::user_name);
+            $client->getGraphs(self::getUserName());
         } catch (\Throwable $e) {
-            $client->createUser(self::token, self::user_name, 'yes','yes');
+            $client->createUser(self::token, self::getUserName(), 'yes','yes');
         }
         $this->assertTrue(true);
     }
@@ -74,7 +98,7 @@ class NormalTest extends TestCase
     public function api_graph ()
     {
         $client = new Client(self::token);
-        $user_name = self::user_name;
+        $user_name = self::getUserName();
 
         $graph_id = "test-graph-01";
         $graph_name = "test-graph";
@@ -118,7 +142,7 @@ class NormalTest extends TestCase
     public function api_pixel ()
     {
         $client = new Client(self::token);
-        $user_name = self::user_name;
+        $user_name = self::getUserName();
 
         $graph_id = "test-graph-01";
         $graph_name = "test-graph";
